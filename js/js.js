2c14b784ab2895cde9a48ad099eca322c3c323f6
Konstantin Chuykov <chuikoff.ru@gmail.com>
2022-11-25 18:03:11 +0600

    add mobile button

--- js/js.js
+++ js/js.js
@@ -1,9 +1,29 @@
-(function() {
-
+(function () {
   const hero = document.querySelector(".header__hero .col-2:last-child");
-  const services = document.getElementById('services');
+  const services = document.getElementById("services");
+  const menuBtn = document.querySelector("#mobile-nav-button button");
+  const navBar = document.getElementById("navbar");
 
-  window.addEventListener('scroll', (e) => {
+  /**
+   * Обрабатываем клик по кнопке вызова меню в мобильной версии
+   */
+  menuBtn.addEventListener("click", (e) => {
+    // Получаем состояние навигационной панели
+    currentState = getComputedStyle(navBar).getPropertyValue("display");
+
+    if (currentState === "none") {
+      navBar.style.setProperty("display", "flex", "important");
+      menuBtn.classList.add("active");
+    } else {
+      navBar.style.setProperty("display", "none", "important");
+      menuBtn.classList.remove("active");
+    }
+  });
+
+  /**
+   * Обрабатываем скролл по странице
+   */
+  window.addEventListener("scroll", (e) => {
     const y = window.pageYOffset;
 
     // Уплывает фон RESUME
@@ -12,12 +32,10 @@
     } else {
       hero.style.backgroundPositionX = "0px";
     }
-  
+
     // Вытягивается фон services
     if (y > 900 && y < 1200) {
-      services.style.backgroundSize = `${y-366}px 453px`;
+      services.style.backgroundSize = `${y - 366}px 453px`;
     }
-    
-  })
-
+  });
 })();
